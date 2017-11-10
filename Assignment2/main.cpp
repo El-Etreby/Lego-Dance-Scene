382c5dd4149afe4fed625d65c119d36d0292cfaa
El-Etreby <omarezzatel-etreby@Omars-MacBook-Pro-7.local>
2017-11-10 10:21:35 +0200

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Omar Ezzat El-Etreby on 11/10/17.
+//  Copyright © 2017 Etro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
